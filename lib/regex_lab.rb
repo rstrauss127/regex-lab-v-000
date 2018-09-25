1bc7d49c715d0cb19b98f1bc22d2385b9f7f535e
Rebecca Strauss <rstrauss127@gmail.com>
2018-09-25 23:54:36 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -2,7 +2,7 @@ require 'pry'
 def starts_with_a_vowel?(word)
   @word = word
 binding.pry
- if @word.match(/^[aeiou] || ^[AEIOU]/) == nil
+ if @word.scan(/^[aeiou]|^[AEIOU]/) == nil
 
    return false
 
