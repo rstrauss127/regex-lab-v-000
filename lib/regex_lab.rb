4c8142b7b2b1ffa391845ea1ca4ea68d3e9ab862
Rebecca Strauss <rstrauss127@gmail.com>
2018-09-26 00:04:36 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,18 +1,14 @@
 require 'pry'
 def starts_with_a_vowel?(word)
-  @word = word
  if word.scan(/^[aeiou]|^[AEIOU]/) == []
-
    return false
-
  else
    return true
  end
-
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\w+ing/)
 end
 
 def words_five_letters_long(text)
