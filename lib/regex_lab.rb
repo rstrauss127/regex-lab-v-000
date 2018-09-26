57dc53837224fd4f7003297578ae4230d8676d41
Rebecca Strauss <rstrauss127@gmail.com>
2018-09-26 00:41:43 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -24,10 +24,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-    if phone.scan(/\D/) == []
-      return false
-    else
-      return true
-    end
-
+  binding.pry
 end
