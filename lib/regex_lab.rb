ef6ebb4e62cd678ed2232ca9585f2d80c9dfa448
Rebecca Strauss <rstrauss127@gmail.com>
2018-09-26 00:46:07 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -24,5 +24,10 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  binding.pry
+  if phone.scan(/[a-z][A-Z]/) == []
+    return true
+  else
+    return false
+  end
+
 end
